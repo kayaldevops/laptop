388fa50df99a3569fdbaf3ac72fde3469d648b14
kayaldevops <kayaswathy@gmail.com>
2017-01-03 22:42:53 +0530

    This is my first commit

---
+++ Demo/src/Demo1.java
@@ -0,0 +1,15 @@
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		int a=5;
+		int b;
+		b=a++;
+		System.out.println(a);
+		System.out.println(b);
+		b=++a;
+		System.out.println(a);
+		System.out.println(b);
+			}
+
+}
